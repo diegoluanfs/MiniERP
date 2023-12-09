d406acacf83a2c9ce9b89e382dfb74efefdb8efe
Diego Silva <diegoluanfs@gmail.com>
2023-12-09 19:39:55 -0300

    feat: foi ajustado o mainform e também a tela do productform

--- MiniERP.Forms/Forms/MainForm.Designer.cs
+++ MiniERP.Forms/Forms/MainForm.Designer.cs
@@ -30,37 +30,39 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.btnCustomer = new System.Windows.Forms.Button();
-            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnSupplier = new System.Windows.Forms.Button();
             this.btnInvoice = new System.Windows.Forms.Button();
             this.lblCustomer = new System.Windows.Forms.Label();
             this.lblProduct = new System.Windows.Forms.Label();
             this.lblInvoice = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnProduct = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCustomer
             // 
             this.btnCustomer.Image = ((System.Drawing.Image)(resources.GetObject("btnCustomer.Image")));
-            this.btnCustomer.Location = new System.Drawing.Point(66, 176);
+            this.btnCustomer.Location = new System.Drawing.Point(168, 64);
             this.btnCustomer.Name = "btnCustomer";
             this.btnCustomer.Size = new System.Drawing.Size(150, 150);
             this.btnCustomer.TabIndex = 0;
             this.btnCustomer.UseVisualStyleBackColor = true;
             this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
             // 
-            // btnProduct
+            // btnSupplier
             // 
-            this.btnProduct.Image = global::MiniERP.Forms.Properties.Resources.supplier_120x120;
-            this.btnProduct.Location = new System.Drawing.Point(303, 176);
-            this.btnProduct.Name = "btnProduct";
-            this.btnProduct.Size = new System.Drawing.Size(150, 150);
-            this.btnProduct.TabIndex = 1;
-            this.btnProduct.UseVisualStyleBackColor = true;
-            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            this.btnSupplier.Image = global::MiniERP.Forms.Properties.Resources.supplier_120x120;
+            this.btnSupplier.Location = new System.Drawing.Point(444, 64);
+            this.btnSupplier.Name = "btnSupplier";
+            this.btnSupplier.Size = new System.Drawing.Size(150, 150);
+            this.btnSupplier.TabIndex = 1;
+            this.btnSupplier.UseVisualStyleBackColor = true;
+            this.btnSupplier.Click += new System.EventHandler(this.btnSupplier_Click);
             // 
             // btnInvoice
             // 
             this.btnInvoice.Image = global::MiniERP.Forms.Properties.Resources.invoice_120x120;
-            this.btnInvoice.Location = new System.Drawing.Point(546, 176);
+            this.btnInvoice.Location = new System.Drawing.Point(444, 268);
             this.btnInvoice.Name = "btnInvoice";
             this.btnInvoice.Size = new System.Drawing.Size(150, 150);
             this.btnInvoice.TabIndex = 2;
@@ -69,7 +71,7 @@
             // lblCustomer
             // 
             this.lblCustomer.AutoSize = true;
-            this.lblCustomer.Location = new System.Drawing.Point(107, 138);
+            this.lblCustomer.Location = new System.Drawing.Point(209, 26);
             this.lblCustomer.Name = "lblCustomer";
             this.lblCustomer.Size = new System.Drawing.Size(61, 20);
             this.lblCustomer.TabIndex = 3;
@@ -78,7 +80,7 @@
             // lblProduct
             // 
             this.lblProduct.AutoSize = true;
-            this.lblProduct.Location = new System.Drawing.Point(344, 138);
+            this.lblProduct.Location = new System.Drawing.Point(469, 26);
             this.lblProduct.Name = "lblProduct";
             this.lblProduct.Size = new System.Drawing.Size(98, 20);
             this.lblProduct.TabIndex = 4;
@@ -87,22 +89,43 @@
             // lblInvoice
             // 
             this.lblInvoice.AutoSize = true;
-            this.lblInvoice.Location = new System.Drawing.Point(592, 138);
+            this.lblInvoice.Location = new System.Drawing.Point(490, 230);
             this.lblInvoice.Name = "lblInvoice";
             this.lblInvoice.Size = new System.Drawing.Size(48, 20);
             this.lblInvoice.TabIndex = 5;
             this.lblInvoice.Text = "Notas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Produtos";
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Image = ((System.Drawing.Image)(resources.GetObject("btnProduct.Image")));
+            this.btnProduct.Location = new System.Drawing.Point(168, 268);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(150, 150);
+            this.btnProduct.TabIndex = 6;
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProduct);
             this.Controls.Add(this.lblInvoice);
             this.Controls.Add(this.lblProduct);
             this.Controls.Add(this.lblCustomer);
             this.Controls.Add(this.btnInvoice);
-            this.Controls.Add(this.btnProduct);
+            this.Controls.Add(this.btnSupplier);
             this.Controls.Add(this.btnCustomer);
             this.Name = "MainForm";
             this.Text = "Main Form";
@@ -114,10 +137,12 @@
         #endregion
 
         private Button btnCustomer;
-        private Button btnProduct;
+        private Button btnSupplier;
         private Button btnInvoice;
         private Label lblCustomer;
         private Label lblProduct;
         private Label lblInvoice;
+        private Label label1;
+        private Button btnProduct;
     }
 }
---
+++ MiniERP.Forms/Forms/ProductForm.cs
@@ -0,0 +1,369 @@
+﻿using MiniERP.Forms.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniERP.Forms.Forms
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+
+            // Adiciona o manipulador de evento para o clique em uma célula do DataGridView
+            dataGridViewProduct.CellClick += DataGridViewProduct_CellClick;
+            // Define o modo de somente leitura para o DataGridView
+            dataGridViewProduct.ReadOnly = true;
+
+            // Adiciona os manipuladores de evento TextChanged para os campos de texto
+            txtSID.TextChanged += TxtCID_TextChanged;
+            txtSName.TextChanged += TxtCName_TextChanged;
+
+            // Adiciona o manipulador de evento KeyPress para aceitar apenas números no txtSID
+            txtSID.KeyPress += TxtCID_KeyPress;
+
+            // Define o texto inicial do botão com base nos valores dos campos de texto
+            UpdateButtonText();
+
+            // Adiciona o manipulador de evento Load
+            this.Load += ProductForm_Load;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Fechar a janela ao clicar em "Voltar"
+            this.Close();
+        }
+
+
+        private async void ProductForm_Load(object sender, EventArgs e)
+        {
+            // Carrega os dados dos clientes no ComboBox cBoxPClient
+            await LoadCustomers();
+
+            // Carrega os dados dos fornecedores no ComboBox cBoxPSupplier
+            await LoadSuppliers();
+        }
+
+        private async Task LoadCustomers()
+        {
+            string apiUrl = "https://localhost:7056/api/customer";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var customers = JsonConvert.DeserializeObject<Customer[]>(await response.Content.ReadAsStringAsync());
+
+                        // Limpa e adiciona os clientes ao ComboBox
+                        cBoxPClient.Items.Clear();
+                        cBoxPClient.DisplayMember = "Name";
+                        cBoxPClient.ValueMember = "CustomerId";
+                        cBoxPClient.Items.AddRange(customers);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro ao carregar clientes: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar clientes: {ex.Message}");
+            }
+        }
+
+        private async Task LoadSuppliers()
+        {
+            string apiUrl = "https://localhost:7056/api/supplier";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var suppliers = JsonConvert.DeserializeObject<Supplier[]>(await response.Content.ReadAsStringAsync());
+
+                        // Limpa e adiciona os fornecedores ao ComboBox
+                        cBoxPSupplier.Items.Clear();
+                        cBoxPSupplier.DisplayMember = "Name";
+                        cBoxPSupplier.ValueMember = "SupplierId";
+                        cBoxPSupplier.Items.AddRange(suppliers);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro ao carregar fornecedores: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar fornecedores: {ex.Message}");
+            }
+        }
+
+        private async void btnMakeProduct_Click(object sender, EventArgs e)
+        {
+            string textProduct = btnMakeProduct.Text;
+
+            if (textProduct == "Listar")
+            {
+                ListAllAsync();
+            }
+            else if (textProduct == "Buscar por Id")
+            {
+                ListByIdAsync(txtSID.Text);
+            }
+            else if (textProduct == "Adicionar")
+            {
+                await AddAsync(txtSName.Text);
+            }
+            else if (textProduct == "Editar")
+            {
+                await EditAsync(txtSID.Text, txtSName.Text);
+            }
+        }
+
+        private async void ListByIdAsync(string id)
+        {
+            string apiUrl = "https://localhost:7056/api/product/" + id;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Deserializa o JSON para um objeto Product
+                        var product = JsonConvert.DeserializeObject<Product>(await response.Content.ReadAsStringAsync());
+
+                        // Verifica se o objeto não é nulo
+                        if (product != null)
+                        {
+                            // Obtém a fonte de dados atual do DataGridView, se existir
+                            var dataSource = dataGridViewProduct.DataSource as List<Product>;
+
+                            // Se não houver uma fonte de dados, crie uma nova lista
+                            if (dataSource == null)
+                            {
+                                dataSource = new List<Product>();
+                            }
+
+                            // Adiciona o novo fornecedor à fonte de dados
+                            dataSource.Add(product);
+
+                            // Atualiza a fonte de dados do DataGridView
+                            dataGridViewProduct.DataSource = null;
+                            dataGridViewProduct.DataSource = dataSource;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Fornecedor não encontrado.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro na requisição: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+
+        private async void ListAllAsync()
+        {
+            string apiUrl = "https://localhost:7056/api/product";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var products = JsonConvert.DeserializeObject<Product[]>(await response.Content.ReadAsStringAsync());
+                        dataGridViewProduct.DataSource = products;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro na requisição: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+
+        private async Task AddAsync(string name)
+        {
+            string apiUrl = "https://localhost:7056/api/product";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Cria um novo objeto Product com o nome fornecido
+                    var newProduct = new Product { Name = name };
+
+                    // Serializa o objeto Product para JSON
+                    string jsonProduct = JsonConvert.SerializeObject(newProduct);
+
+                    // Cria um conteúdo JSON para a requisição
+                    var content = new StringContent(jsonProduct, Encoding.UTF8, "application/json");
+
+                    // Envia a requisição HTTP POST para adicionar um novo fornecedor
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Fornecedor adicionado com sucesso.");
+                        // Atualiza a lista de fornecedores após adicionar um novo fornecedor
+                        txtSName.Text = "";
+                        txtSID.Text = "";
+                        btnClean.Enabled = false;
+
+                        ListAllAsync();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro na requisição: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+
+        private async Task EditAsync(string id, string name)
+        {
+            string apiUrl = "https://localhost:7056/api/product/";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Cria um objeto Product com o ID e o novo nome fornecidos
+                    var updatedProduct = new Product { ProductId = int.Parse(id), Name = name };
+
+                    // Serializa o objeto Product para JSON
+                    string jsonProduct = JsonConvert.SerializeObject(updatedProduct);
+
+                    // Cria um conteúdo JSON para a requisição
+                    var content = new StringContent(jsonProduct, Encoding.UTF8, "application/json");
+
+                    // Envia a requisição HTTP PUT para editar o fornecedor existente
+                    HttpResponseMessage response = await client.PutAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Fornecedor editado com sucesso.");
+                        // Atualiza a lista de fornecedors após editar um fornecedor existente
+                        txtSName.Text = "";
+                        txtSID.Text = "";
+                        btnClean.Enabled = false;
+
+                        ListAllAsync();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Erro na requisição: {response.StatusCode} - {response.ReasonPhrase} \n "+ content);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+
+        // Manipulador de evento para o clique em uma célula do DataGridView
+        private void DataGridViewProduct_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Verifica se o clique foi em uma célula válida e não no cabeçalho
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                // Obtém os dados da linha clicada
+                DataGridViewRow row = dataGridViewProduct.Rows[e.RowIndex];
+                txtSID.Text = row.Cells["ProductId"].Value.ToString();
+                txtSName.Text = row.Cells["Name"].Value.ToString();
+            }
+        }
+
+        private void TxtCID_TextChanged(object sender, EventArgs e)
+        {
+            UpdateButtonText();
+        }
+
+        private void TxtCName_TextChanged(object sender, EventArgs e)
+        {
+            UpdateButtonText();
+        }
+        private void UpdateButtonText()
+        {
+            bool isCIDEmpty = string.IsNullOrWhiteSpace(txtSID.Text);
+            bool isCNameEmpty = string.IsNullOrWhiteSpace(txtSName.Text);
+
+            if (isCIDEmpty && isCNameEmpty)
+            {
+                btnMakeProduct.Text = "Listar";
+            }
+            else if (!isCIDEmpty && isCNameEmpty)
+            {
+                btnMakeProduct.Text = "Buscar por Id";
+            }
+            else if (isCIDEmpty && !isCNameEmpty)
+            {
+                btnMakeProduct.Text = "Adicionar";
+            }
+            else
+            {
+                btnMakeProduct.Text = "Editar";
+            }
+
+            // Habilita ou desabilita o botão btnClean com base nos campos txtSID e txtSName
+            btnClean.Enabled = !isCIDEmpty || !isCNameEmpty;
+        }
+
+        private void TxtCID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permite apenas números e tecla Backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtSName.Text = "";
+            txtSID.Text = "";
+
+            // Atualiza o estado do botão btnClean
+            UpdateButtonText();
+        }
+
+    }
+}
